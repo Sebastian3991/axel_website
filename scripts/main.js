cbdee691f86251b22db507ced09db6faf00500d4
Sebastian Sebastian <sebastian.schoedl@gmail.com>
2020-08-27 01:50:54 +0200

    scroll event listener

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
-function myFunction(x) {
+//check for landing page
+if(document.getElementById('landing-page-check')) {
+	  //load home page when scrolled
+	  document.body.addEventListener('wheel', () => {redirectLanding()}, false);
+}
+
+//landing page exit animation to home page  /////////hier gehts weiter oder nav bar
+window.onbeforeunload = function(e){
+    document.getElementById('myDiv').className = 'out';
+}
+
+function redirectLanding(){
+	window.location.href = 'self.html';
+}
+
+function openToCloseNav(x) {
   x.classList.toggle("change");
 }
+
+
